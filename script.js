0c92ef19a9e8b7cc234c2874b3641f3ce2c2855f
Karthikeya.Gundumogula <karthikeya200226@gmail.com>
2022-10-04 14:13:20 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   .env
    	modified:   script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const Moralis = require("moralis-v1")
+require("dotenv").config()
+
+const serverId=process.env.SERVERURL
+const appId=process.env.appId
+
+Moralis.start({serverId,appId})
+
+const profile=document.getElementById("profile")
