bd82c4c065c5c7f9d34ae4988d3185c3e8d78fbb
Karthikeya.Gundumogula <57733888+KarthikeyaGundumogula@users.noreply.github.com>
2022-10-04 19:30:35 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   index.html
    	renamed:    index.js -> script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const Moralis = require("moralis-v1");
 require("dotenv").config();
 const url = process.env.SERVERURL;
 const appId = process.env.APPID;
-let profile = document.getElementById("profile");
 
 Moralis.start({ serverUrl: url, appId: appId });
 
@@ -22,5 +21,4 @@ async function connect() {
     profile.textContent = "Connected";
   }
 }
-
-profile.onclick = connect;
+document.getElementById("profile").onclick = connect;
